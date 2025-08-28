a9dead5626966d52a3e4988a6e650064e725cae7
Siranjeevan <smartjeevith2k@gmail.com>
2025-08-28 19:51:49 +0530

    Image

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Summa from './summa.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Summa />
   </StrictMode>,
 )
---
+++ src/summa.jsx
@@ -0,0 +1,9 @@
+import Image from "../public/1.jpg"
+function ImageLayout(){
+    return(
+        <>
+            <img src={Image}  className="h-140" />
+        </>
+    );
+}
+export default ImageLayout;
